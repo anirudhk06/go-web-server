884502a060a6371b0699ef5558455fed8cdf6215
anirudhk06 <anirudhani300@gmail.com>
2024-12-05 23:41:34 +0530

    login init

--- controllers/authController.go
+++ controllers/authController.go
@@ -2,10 +2,12 @@ package controllers
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/anirudhk06/go-web-server/configs"
 	"github.com/anirudhk06/go-web-server/models"
 	"github.com/anirudhk06/go-web-server/utils"
+	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -46,4 +48,39 @@ func Register(w http.ResponseWriter, r *http.Request) {
 
 func Login(w http.ResponseWriter, r *http.Request) {
 
+	var payload struct {
+		Email    string `json:"email,required"`
+		Password string `json:"password"`
+	}
+
+	if err := utils.ParseJSON(r, &payload); err != nil {
+		utils.WriteJSON(w, map[string]string{"detail": "Invalid json data"}, http.StatusBadRequest)
+		return
+	}
+
+	user := models.User{}
+	err := configs.DB.Where("email = ?", payload.Email).First(&user).Error
+
+	if err != nil {
+		utils.WriteJSON(w, map[string]string{"detail": "Invalid credentials"}, http.StatusBadRequest)
+		return
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(payload.Password))
+
+	if err != nil {
+		utils.WriteJSON(w, map[string]string{"detail": "Invalid credentials"}, http.StatusBadRequest)
+		return
+	}
+
+	claims := jwt.NewWithClaims(jwt.SigningMethodES256, jwt.MapClaims{
+		"sub": user.ID,
+		"exp": time.Now().Add(time.Second * 10).Unix(),
+		"iat": time.Now().Unix(),
+	})
+
+	utils.WriteJSON(w, map[string]string{
+		"access": "",
+		"refrsh": "",
+	}, http.StatusOK)
 }
