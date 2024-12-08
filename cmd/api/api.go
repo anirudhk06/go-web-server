5b09037311c1702c69fd4dbde8fa23140732fb02
anirudhk06 <anirudhani300@gmail.com>
2024-12-08 20:31:49 +0530

    created middleware stack

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/anirudhk06/go-web-server/middleware"
 	"github.com/anirudhk06/go-web-server/service/user"
 	"gorm.io/gorm"
 )
@@ -30,5 +31,14 @@ func (s *APIServer) Run() error {
 
 	mux.Handle("/api/v1/", http.StripPrefix("/api/v1", v1))
 	fmt.Printf("Server is running on port: %s\n", s.addr)
-	return http.ListenAndServe(":"+s.addr, mux)
+
+	middlewareStack := middleware.CreateStack(
+		middleware.Logging,
+	)
+
+	server := http.Server{
+		Addr:    ":" + s.addr,
+		Handler: middlewareStack(mux),
+	}
+	return server.ListenAndServe()
 }
