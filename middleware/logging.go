dae43028f40a8892b48e98ceab5099ebb87b1a23
anirudhk06 <anirudhani300@gmail.com>
2024-12-09 11:50:25 +0530

    added status code to the loggin middleware

--- middleware/logging.go
+++ middleware/logging.go
@@ -6,10 +6,27 @@ import (
 	"time"
 )
 
+type WrappedWritter struct {
+	http.ResponseWriter
+	StatusCode int
+}
+
+func (w *WrappedWritter) WriteHeader(statusCode int) {
+	w.ResponseWriter.WriteHeader(statusCode)
+	w.StatusCode = statusCode
+}
+
 func Logging(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
-		next.ServeHTTP(w, r)
-		log.Println(r.Method, r.URL.Path, time.Since(start))
+
+		wrapped := &WrappedWritter{
+			ResponseWriter: w,
+			StatusCode:     http.StatusOK,
+		}
+
+		next.ServeHTTP(wrapped, r)
+
+		log.Println(r.Method, r.URL.Path, wrapped.StatusCode, time.Since(start))
 	})
 }
